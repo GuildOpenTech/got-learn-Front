071c325999c18f057017867ee805308aa4c0a3f7
combesal <138445070+combesal@users.noreply.github.com>
2024-06-28 14:12:38 +0200

    ajouter le logo au header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,29 @@
+import { Colors } from "../core/colors";
 import Navbar from "./NavBar/Navbar";
 
 export default function Header() {
 
   return (
-    <header>
-      <img 
-        src="/src/assets/react.svg"
-        alt="image ici" 
+    <header style={{
+      width: "100 %",
+      height: "130px",
+      position: "sticky",
+      display: "flex",
+      alignItems:  "center",
+      justifyContent: "space-between",
+      top: 0,
+      background:Colors.BLEU_NUIT
+    }}>
+      <img
+        onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth', })}}
+        src="/src/assets/LOGO_TxtBlc_99px.svg"
+        alt="Logo Guild Open Tech"
         style={{
-          width: '100px'
+          width: '99px',
+          marginLeft: "58px",
         }}
       />
       <Navbar />
-    </header>
-  ) 
+    </header >
+  )
 }
