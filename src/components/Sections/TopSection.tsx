7c3c0023ad92ad4d2f7f1caf686bad4cb35b1f67
qjacquet <q.jacquet@gmail.com>
2024-06-29 03:20:08 +0200

    wip

--- src/components/Sections/TopSection.tsx
+++ src/components/Sections/TopSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { Colors } from "../../core/colors";
 
 export default function TopSection() {
@@ -14,7 +14,8 @@ export default function TopSection() {
       }}
     >
       <TitleAndLogo />
-      <Content/>
+      <Description />
+      <CTAButton />
     </Box>
   )
 }
@@ -90,48 +91,72 @@ const TitleAndLogo = () => {
   )
 }
 
-const Content = () => {
+const CTAButton = () => {
   return (
-    <>
-      <Box
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Button
+        variant="contained"
         sx={{
-          position: 'absolute',
-          left: '175px',
-          marginTop: '64px',
-          width: '2.3px',
-          height: '160px',
-          background: 'white',
-        }}
-      />
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'flex-start',
-          gap: '24px',
-          position: 'absolute',
-          left: '170px',
-          marginTop: '48px'
+          background: 'radial-gradient(170.24% 336.33% at 100% 84.88%, #0D2D45 0%, #0D2D45 38.42%, #2AB8CD 100%)',
         }}
       >
-        <MarkerWithText
-          text="Participe gratuitement à des projets open source technologiquement innovants"
-        />
+        Contained
+      </Button>
+    </Box>
 
-        <MarkerWithText
-          color={Colors.ORANGE}
-          text="Immerge toi dans la GOT Âme, inspirée par l'École 42 et notre passion pour l'IT"
-        />
+  )
+}
 
-        <MarkerWithText
-          text="Expérimenté ou en reconversion, réseaute avec des experts dans leurs domaines"
-        />
+const MarkersLine = () => {
+  return (
+    <Box
+      sx={{
+        position: 'absolute',
+        marginLeft: '175px',
+        marginTop: '16px',
+        width: '2.3px',
+        height: '160px',
+        background: 'white',
+      }}
+    />
+  )
+}
 
-        <MarkerWithText
-          color={Colors.ORANGE}
-          text="Collabore avec des passionnés solidaires, acquiers de l'expérience, monte en compétences"
-        />
-      </Box>
-    </>
+const Description = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        gap: '24px',
+        marginLeft: '170px',
+        marginTop: '48px',
+      }}
+    >
+      <MarkerWithText
+        text="Participe gratuitement à des projets open source technologiquement innovants"
+      />
+
+      <MarkerWithText
+        color={Colors.ORANGE}
+        text="Immerge toi dans la GOT Âme, inspirée par l'École 42 et notre passion pour l'IT"
+      />
+
+      <MarkerWithText
+        text="Expérimenté ou en reconversion, réseaute avec des experts dans leurs domaines"
+      />
+
+      <MarkerWithText
+        color={Colors.ORANGE}
+        text="Collabore avec des passionnés solidaires, acquiers de l'expérience, monte en compétences"
+      />
+    </Box>
   )
 }
