19b6b62099085813769fe19918b4146fbc838c99
combesal <138445070+combesal@users.noreply.github.com>
2024-07-01 15:52:54 +0200

    Changement image + ajout url

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,46 @@
+import Box from "@mui/material/Box";
 import { Colors } from "../core/colors";
 import Navbar from "./NavBar/Navbar";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (location.pathname === '/') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      navigate('/');
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
 
   return (
-    <header style={{
-      width: "100 %",
-      height: "130px",
-      position: "sticky",
-      display: "flex",
-      alignItems:  "center",
-      justifyContent: "space-between",
-      top: 0,
-      background:Colors.BLEU_NUIT
-    }}>
+    <Box
+      component="header"
+      bgcolor={Colors.BLEU_NUIT}
+      sx={{
+        width: '100 %',
+        height: '130px',
+        position: 'sticky',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        top: 0,
+      }}
+    >
       <img
-        onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth', })}}
-        src="/src/assets/LOGO_TxtBlc_99px.svg"
+        onClick={handleClick}
+        src="/src/assets/LOGO_TxtBlc_99px.png"
         alt="Logo Guild Open Tech"
         style={{
           width: '99px',
           marginLeft: "58px",
+          cursor: 'pointer',
         }}
       />
       <Navbar />
-    </header >
+    </Box>
   )
 }
